d1c33292739ddf4d1f2f535f46f74382aa029306
Prashantkr81 <Prashantkr81@outlook.com>
2026-07-13 23:01:34 +0530

    Controller

--- finance-management/src/main/java/com/prashant/finance_management/controller/TransactionController.java
+++ finance-management/src/main/java/com/prashant/finance_management/controller/TransactionController.java
@@ -1,9 +1,56 @@
 package com.prashant.finance_management.controller;
 
 
+import com.prashant.finance_management.dto.TransactionRequestDTO;
+import com.prashant.finance_management.dto.TransactionResponseDTO;
+import com.prashant.finance_management.service.TransactionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/transactions")
 public class TransactionController {
 
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService= transactionService;
+    }
+
+    @PutMapping
+    public ResponseEntity<TransactionResponseDTO> createTransaction(
+            @RequestBody TransactionRequestDTO requestDTO
+    ){
+        TransactionResponseDTO response = transactionService.createTransaction(requestDTO);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<TransactionResponseDTO> getTransactionById(@PathVariable Long id){
+
+        TransactionResponseDTO response= transactionService.getTransactionById(id);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TransactionResponseDTO> updateTransaction(
+            @PathVariable Long id,
+            @RequestBody TransactionRequestDTO requestDTO) {
+
+        return ResponseEntity.ok(
+                transactionService.updateTransaction(id, requestDTO)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTransaction(
+            @PathVariable Long id) {
+
+        transactionService.deleteTransaction(id);
+
+        return ResponseEntity.ok("Transaction deleted successfully.");
+    }
 }
